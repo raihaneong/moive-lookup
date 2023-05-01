e7252d004ceb4678466489c4d307e63a22103918
raihaneong <raihneong@gmail.com>
2023-05-01 17:29:08 +0700

    Not using Link for the not-found file

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,8 @@
-import Link from 'next/link'
-
 export default function NotFound() {
     return (
         <div className="p-1">
             <h1>not found</h1>
-            <Link href="/"> back home</Link>
+            <a href="/"> back home</a>
         </div>
         )
 }
