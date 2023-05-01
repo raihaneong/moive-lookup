89910dd3750f8a1918096280018c92da9fb65dbd
raihaneong <raihneong@gmail.com>
2023-05-01 15:57:45 +0700

    Reset main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="p-1">
       <button className="rounded-lg duration-500 p-1 ring-2 ring-purple-500 ring-offset-4 ring-offset-slate-50 dark:ring-offset-slate-900 hover:bg-purple-700 hover:text-xl">MENU</button>
       <footer>
         <Image src="/themoviedb-logoattribution.svg" alt="themoviedb logo" width={100} height={100}/>
