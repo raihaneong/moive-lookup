3ff49cba06cf62a4e3ca9332890483e010e21441
raihaneong <raihneong@gmail.com>
2023-05-01 12:23:36 +0700

    Add button and basic styling for menu button

--- app/page.tsx
+++ app/page.tsx
@@ -111,6 +111,7 @@ export default function Home() {
           </p>
         </a>
       </div>
+      <button className="rounded-lg duration-500 p-1 ring-2 ring-purple-500 ring-offset-4 ring-offset-slate-50 dark:ring-offset-slate-900 hover:bg-purple-700 hover:text-xl">MENU</button>
       <footer>
         <Image src="/themoviedb-logoattribution.svg" alt="themoviedb logo" width={100} height={100}/>
       </footer>
