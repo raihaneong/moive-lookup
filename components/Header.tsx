72a89e66ab1786bc0e45bd755c824f09b4db2207
raihaneong <raihneong@gmail.com>
2023-05-01 18:33:21 +0700

    Decrease padding

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <section className="text-white">
-      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:items-center">
+      <div className="mx-auto max-w-screen-xl px-4 py-24 lg:flex lg:items-center">
         <div className="mx-auto max-w-3xl text-center selection:bg-fuchsia-300 selection:text-fuchsia-900">
           <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
             Search your favorite movie,
