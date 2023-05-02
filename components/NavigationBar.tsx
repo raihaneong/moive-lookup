d24f46022edf90fb3092c3c4805a43bfba81f7bd
raihaneong <raihneong@gmail.com>
2023-05-02 16:58:56 +0700

    Small menu

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,10 +1,10 @@
 export default function NavigationBar() {
   return (
-    <nav aria-label="Main Nav" className="flex flex-col">
+    <nav aria-label="Main Nav" className="flex">
       <details className="group [&_summary::-webkit-details-marker]:hidden">
         <summary className="group flex items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
           <div className="flex items-center gap-2">
-            <span className="text-sm font-medium"> MENU </span>
+            <span className="text-sm font-medium pr-4"> MENU </span>
           </div>
 
           <span className="shrink-0 transition duration-300 group-open:-rotate-180">
