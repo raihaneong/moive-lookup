de22121637edb64deedbc5abb8b9ae977ca82a50
raihaneong <raihneong@gmail.com>
2023-05-02 16:37:56 +0700

    Adding vertical dropdown for navigation bar

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,7 +1,62 @@
 export default function NavigationBar() {
-    return (
-        <div>
-            <button className="rounded-lg duration-500 p-1 ring-2 ring-purple-500 ring-offset-4 ring-offset-slate-50 dark:ring-offset-slate-900 hover:bg-purple-700 hover:text-xl">MENU</button>
-        </div>
-    )
+  return (
+    <nav aria-label="Main Nav" className="flex flex-col">
+      <details className="group [&_summary::-webkit-details-marker]:hidden">
+        <summary className="group flex items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+          <div className="flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 opacity-75"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+              />
+            </svg>
+
+            <span className="text-sm font-medium"> Teams </span>
+          </div>
+
+          <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </span>
+        </summary>
+
+        <nav
+          aria-label="Users Nav"
+          className="mt-2 flex flex-col space-y-1 px-4"
+        >
+          <a
+            href=""
+            className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+          >
+            Banned Users
+          </a>
+
+          <a
+            href=""
+            className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+          >
+            Calendar
+          </a>
+        </nav>
+      </details>
+    </nav>
+  );
 }
