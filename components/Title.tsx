c5a7b0c1935717a2b4634a7a52fb4f62d63999d3
raihaneong <raihneong@gmail.com>
2023-05-02 16:25:41 +0700

    Center the home page

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,7 +1,7 @@
 export default function Title() {
   return (
     <header aria-label="Page Header">
-      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-8 lg:px-6">
         <div className="sm:flex sm:items-center sm:justify-between">
           <div className="text-center sm:text-left">
             <h1 className="text-2xl font-bold text-white-900 sm:text-3xl">
