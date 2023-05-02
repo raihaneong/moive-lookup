cf6b51265c53f0a4c91e97ec6a44c73f9c3427c6
raihaneong <raihneong@gmail.com>
2023-05-02 16:49:31 +0700

    Modify navigation bar

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -4,22 +4,7 @@ export default function NavigationBar() {
       <details className="group [&_summary::-webkit-details-marker]:hidden">
         <summary className="group flex items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
           <div className="flex items-center gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 opacity-75"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
-              />
-            </svg>
-
-            <span className="text-sm font-medium"> Teams </span>
+            <span className="text-sm font-medium"> MENU </span>
           </div>
 
           <span className="shrink-0 transition duration-300 group-open:-rotate-180">
@@ -43,17 +28,17 @@ export default function NavigationBar() {
           className="mt-2 flex flex-col space-y-1 px-4"
         >
           <a
-            href=""
+            href="/search"
             className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
           >
-            Banned Users
+            Search
           </a>
 
           <a
             href=""
             className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
           >
-            Calendar
+            About
           </a>
         </nav>
       </details>
