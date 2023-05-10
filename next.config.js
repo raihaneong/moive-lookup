f933d3ff7cbb875cd0cdae9d65506e9a1f19b356
raihaneong <raihneong@gmail.com>
2023-05-10 16:18:14 +0700

    Allowing image from tecdn website

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
+const { hostname } = require('os');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'tecdn.b-cdn.net'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
