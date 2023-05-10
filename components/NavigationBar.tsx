154748028220b66ae1d73fec68be95c6204bec46
raihaneong <raihneong@gmail.com>
2023-05-10 16:29:10 +0700

    Routing to about page

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -36,7 +36,7 @@ export default function NavigationBar() {
             </a>
 
             <a
-              href=""
+              href="/about"
               className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
             >
               About
