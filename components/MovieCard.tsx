13082b42377e7b79af447148d49ac0be673c79f1
raihaneong <raihneong@gmail.com>
2023-05-10 16:27:11 +0700

    Adapting to environment

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -69,6 +69,83 @@ export default function MovieCard() {
               height={300}
             />
           </div>
+              </div>
+              <div className="flex w-1/3 flex-wrap">
+          <div className="w-full p-1 md:p-2">
+            <Image
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+              width={500}
+              height={300}
+            />
+          </div>
+              </div>
+              <div className="flex w-1/3 flex-wrap">
+          <div className="w-full p-1 md:p-2">
+            <Image
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+              width={500}
+              height={300}
+            />
+          </div>
+              </div>
+              <div className="flex w-1/3 flex-wrap">
+          <div className="w-full p-1 md:p-2">
+            <Image
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+              width={500}
+              height={300}
+            />
+          </div>
+              </div>
+              <div className="flex w-1/3 flex-wrap">
+          <div className="w-full p-1 md:p-2">
+            <Image
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+              width={500}
+              height={300}
+            />
+          </div>
+              </div>
+              <div className="flex w-1/3 flex-wrap">
+          <div className="w-full p-1 md:p-2">
+            <Image
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+              width={500}
+              height={300}
+            />
+          </div>
+              </div>
+              <div className="flex w-1/3 flex-wrap">
+          <div className="w-full p-1 md:p-2">
+            <Image
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+              width={500}
+              height={300}
+            />
+          </div>
+              </div>
+              <div className="flex w-1/3 flex-wrap">
+          <div className="w-full p-1 md:p-2">
+            <Image
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+              width={500}
+              height={300}
+            />
+          </div>
         </div>
       </div>
     </div>
