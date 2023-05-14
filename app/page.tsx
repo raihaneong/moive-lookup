9dc68808e43fe22e0cd24389b2883cfeee5fa633
raihaneong <raihneong@gmail.com>
2023-05-14 12:32:12 +0700

    Deleting state related code

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useState } from 'react'
 
 import SeachBar from 'components/SearchBar'
 import Footer from 'components/Footer'
@@ -9,7 +8,6 @@ import Header from 'components/Header'
 
 export default function Home() {
 
-  const [search, setSearch] = useState('')
 
   return (
     <div className="flex-col m-4">
