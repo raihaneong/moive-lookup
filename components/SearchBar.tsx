36807465615b848d6a6700604ab0345003104fa5
raihaneong <raihneong@gmail.com>
2023-05-14 20:18:17 +0700

    Input form

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -38,7 +38,7 @@ export default function SeachBar() {
             </span>
             <input
               type="search"
-              name="Search"
+              name="pooouu"
               placeholder="Search..."
               onChange={handleChange}
               className="w-32 py-2 pl-10 text-sm rounded-md sm:w-auto focus:outline-none dark:bg-gray-800 dark:text-gray-100 focus:dark:bg-gray-900 focus:dark:border-violet-400"
