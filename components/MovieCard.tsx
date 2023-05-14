1d64f8a7ed9f44687e44c3a48789b16082915514
raihaneong <raihneong@gmail.com>
2023-05-14 20:05:14 +0700

    Something wrong occured with these lines

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,19 +1,10 @@
-'use client';
+
 import Image from "next/image";
 
 import { useMovieStore } from "@/lib/searchTerm";
-
-const callAPI = async (search: string) => {
-  const res = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=${process.env.API_KEY}&language=en-US&query=${search}&page=1&include_adult=false`)
-  if (!res.ok) {
-    throw new Error(res.statusText)
-  }
-  console.log(res.json())
-}
 export default function MovieCard() {
 
   const search = useMovieStore(state => state.search);
-
   return (
     <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
       <div className="-m-1 flex flex-wrap md:-m-2">
@@ -27,7 +18,7 @@ export default function MovieCard() {
               height={300}
             />
           </div>
-          <h1>{ search}</h1>
+          <h1>{search}</h1>
         </div>
       </div>
     </div>
