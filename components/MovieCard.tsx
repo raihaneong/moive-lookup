1b72c79047404bff4a72e00f262dea2f7211b8cc
rhn <raihneong@gmail.com>
2023-06-03 02:09:06 +0700

    Revert "Delete line that contained zustand"
    This reverts commit 50d01670baf22400d4985a73cae6590bf9653864.

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,8 +1,10 @@
 'use client'
 import Image from "next/image";
 
+import { useSearhTerm } from "@/lib/searchTerm";
 export default function MovieCard() {
 
+  const search = useSearhTerm(state => state.search);
 
   
   return (
